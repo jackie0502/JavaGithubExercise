15d6794b3d0f8c0e9bfc26f48b5756070df788ed
aquavit8584 <75455482+aquavit8584@users.noreply.github.com>
2025-04-28 18:52:58 +0800

    feat: third version-print the third member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!"); 
         System.out.println("\"學號：\"01257113\"\n姓名：\"郭家齊\""); 
-        System.out.println("\"學號：\"01257122\"\n姓名：\"郭晉佑\""); 
+        System.out.println("\"學號：\"01257122\"\n姓名：\"郭晉佑\"");
+        System.out.println("\"學號：\"01257106\"\n姓名：\"褚宥辰\"");
     }
 }
