da286a96e2f4da8da896cd7c54e0aa98853e4d53
YowDesert <hpes509@gmail.com>
2025-04-28 18:01:22 +0800

    second version-print the second member name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!"); 
         System.out.println("\"學號：\"01257113\"\n姓名：\"郭家齊\""); 
+        System.out.println("\"學號：\"01257122\"\n姓名：\"郭晉佑\""); 
     }
 }
